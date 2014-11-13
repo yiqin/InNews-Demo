2f0ade23f3b968741064e83148bfb5dc972e5a68
yiqin <yiqin.mems@gmail.com>
2014-11-13 15:32:01 -0800

    Add the regular article.

---
+++ InNews-Demo/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  InNews-Demo
+//
+//  Created by yiqin on 11/13/14.
+//  Copyright (c) 2014 yiqin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
