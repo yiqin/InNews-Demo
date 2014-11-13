cf36f99656fa55a925e47c511edc1c842211c87a
yiqin <yiqin.mems@gmail.com>
2014-11-13 15:01:43 -0800

    Finish init method.

--- InNews-Demo/AppDelegate.m
+++ InNews-Demo/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "InNews.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,10 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    [InNews setApplicationId:@"OwnvY79qyoEj4FcUgx5YM79Df" apiKey:@"biUxaCvg6G"];
+    
+    
     return YES;
 }
 
--- InNews/InNews.m
+++ InNews/InNews.m
@@ -7,7 +7,67 @@
 //
 
 #import "InNews.h"
+#import "YQParse.h"
+
+@interface InNews ()
+
+@property(nonatomic, strong) NSString *applicationId;
+@property(nonatomic, strong) NSString *apiKey;
+
+@end
+
 
 @implementation InNews
+
++ (instancetype)sharedManager
+{
+    static InNews *sharedManager = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedManager = [[self alloc] init];
+    });
+    return sharedManager;
+}
+
+- (instancetype)init
+{
+    if (self = [super init]) {
+        _applicationId = @"";
+        _apiKey = @"";
+    }
+    return self;
+}
+
++ (void)setApplicationId:(NSString*)applicationId apiKey:(NSString*)apiKey
+{
+    // Parse Authorization
+    [YQParse setApplicationId:@"WpVs4S6jbgi7KoLIDi6YAnuBr6MMBn6HfeiD30Vn"
+                   restApiKey:@"WSwpkuQWjwJwaDYb0zKUj9CzRuKZP9rgkxsPj2pY"];
+    
+    NSLog(@"*************************************");
+    NSLog(@"**********     0.1.0     ************");
+    NSLog(@"****** Update date: 11/13/2015 ******");
+    NSLog(@"*************************************");
+    
+    InNews *shared = [InNews sharedManager];
+    shared.applicationId = applicationId;
+    shared.apiKey = apiKey;
+}
+
++ (NSString*)getApplicationId
+{
+    InNews *shared = [InNews sharedManager];
+    return shared.applicationId;
+}
+
++ (NSString*)getApiKey
+{
+    InNews *shared = [InNews sharedManager];
+    return shared.apiKey;
+}
+
+
+
+
 
 @end
