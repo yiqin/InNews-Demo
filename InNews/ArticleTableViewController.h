4a74790f9a870f0384470354e10dc97c02de1ee2
yiqin <yiqin.mems@gmail.com>
2014-11-14 01:26:51 -0800

    Add Ads Cell.

--- InNews/ArticleTableViewController.h
+++ InNews/ArticleTableViewController.h
@@ -7,8 +7,9 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "SWTableViewCell.h"
 
-@interface ArticleTableViewController : UITableViewController
+@interface ArticleTableViewController : UITableViewController <SWTableViewCellDelegate>
 
 
 @property(strong, nonatomic) NSMutableDictionary *blocks;
